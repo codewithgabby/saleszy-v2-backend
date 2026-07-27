4b89fc8d37c5cf627a93e9e14d310b701d2a99a6
Gabriel Johnson <johnfem4real@gmail.com>
2026-07-27 20:52:51 +0100

    fix shift

--- app/repositories/shift_repository.py
+++ app/repositories/shift_repository.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.orm import Session
 from sqlalchemy import func
 from app.models import Shift, ShiftEvent, Sale, Return, User
 from typing import Optional, List
@@ -108,7 +108,6 @@ class ShiftRepository:
 
         query = (
             db.query(Shift)
-            .options(joinedload(Shift.user))
             .filter(Shift.business_id == business_id)
         )
 
